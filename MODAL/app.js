994ca7694427655919bebff26f6efecfb4a583ad
MudassirRaza12345 <mudassirraza12345@gmail.com>
2020-11-11 10:05:14 -0800

    first commit

---
+++ MODAL/app.js
@@ -0,0 +1,9 @@
+function big(e){
+    var modalimage=document.getElementById("modalimg")
+    modalimage.src=e.src
+    modalimage.style.width="850px"
+    modalimage.style.height="400px"
+    console.log(modalimage)
+    
+     }
+    
